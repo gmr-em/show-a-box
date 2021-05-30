325acb7b1ad1e445fe084b93a43edd5fd71140f7
MariusG <gheorghemr@gmail.com>
2021-05-30 16:56:29 +0200

    cleaned up code; added lights; made bit nicer looking

--- show-a-box/show-a-box.component.ts
+++ show-a-box/show-a-box.component.ts
@@ -23,66 +23,45 @@ export class ShowABoxComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
       this.lines = new Array<Array<THREE.Line>>();
-      this.generateCorners();
-      this.showABox_v2();
+      this.showABox();
   }
 
   ngOnDestroy() {
     cancelAnimationFrame(this.reqId);
   }
 
-  showBox() {
-      var scene = new THREE.Scene();
-			var camera = new THREE.PerspectiveCamera( 75, 1, 0.1, 1000 );
-      let controls = new OrbitControls(camera, this.renderer_canvas.nativeElement);
-			var renderer = new THREE.WebGLRenderer({canvas: this.renderer_canvas.nativeElement});
-      renderer.setSize(800, 800);
-      var geometry = new THREE.BoxGeometry();
-      
-			var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-			var cube = new THREE.Mesh( geometry, material );
-			scene.add( cube );
-
-			camera.position.z = 5;
-
-			var animate = function () {        
-				requestAnimationFrame( animate );
-
-				cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
-        console.log(camera.position);
-
-				renderer.render( scene, camera );
-			};
-
-			animate();
-  }
-
-  showABox_v2() {
+  showABox() {
     var scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0x809090 );
+    // scene.fog = new THREE.Fog( 0xcce0ff, 500, 10000 );
+
     this.camera = new THREE.PerspectiveCamera( 75, 1, 0.1, 1000 );
-    this.camera.position.set( 0, 0, 100 );
+    this.camera.position.set( 40, 40, 40 );
     this.camera.lookAt( 0, 0, 0 );
     let controls = new OrbitControls(this.camera, this.renderer_canvas.nativeElement);
-    var renderer = new THREE.WebGLRenderer({canvas: this.renderer_canvas.nativeElement});
+    var renderer = new THREE.WebGLRenderer({canvas: this.renderer_canvas.nativeElement, antialias: true});
     renderer.setSize(800, 800);
 
     this.addCube(scene);
-    this.addVanishingLines(scene, 0);
-    this.addVanishingLines(scene, 6);
+    this.addVanishingLines(scene);
 
-    this.lines.forEach((lines)=>lines.forEach(line => scene.add(line)));
+    let ambientLight = new THREE.AmbientLight(0x808080);
+    scene.add(ambientLight);
     
-    // scene.add(new THREE.AxesHelper(30));
+    let light = new THREE.DirectionalLight(0xF0F0F0);
+    light.position.set(50, 200, 100);
+    scene.add(light);
+
+    scene.add(new THREE.AxesHelper(30));
     let currentMin = -1;
     var animate = () => {
-      console.log("Hello");
-      this.reqId = requestAnimationFrame( animate );
+      this.reqId = requestAnimationFrame( animate );     
       
-      let minIndex = this.getClosestCornerToCamera();
+      let minIndex = this.getCameraQuadrant();
       if (minIndex != currentMin) {
         console.log(this.camera.position);
         console.log(`CHANGED TO ${minIndex}`);
+
         currentMin = minIndex;
         this.hideLines(currentMin);
       }
@@ -92,106 +71,84 @@ export class ShowABoxComponent implements OnInit, OnDestroy {
     animate();
   }
 
-  addPlane(scene: THREE.Scene) {
-    var geometry = new THREE.PlaneBufferGeometry( 10, 10, 10 );
-    var material = new THREE.MeshBasicMaterial( {color: 0xff0000, side: THREE.DoubleSide} );
-    var plane = new THREE.Mesh( geometry, material );
-    console.log(plane.position);
-    plane.position.set(5,5,0);
-    console.log(plane.position);
-    scene.add( plane );
-  }
-
-  addVanishingLines(scene: THREE.Scene, selectedPoint_thingy: number) {
+  addVanishingLines(scene: THREE.Scene) {
     // array with the lines towards the vanishing points (Array of 3-> Array of 4 lines -> Array of points)
-    
+    let cubeOffset = 10;
     let points = [
                 new THREE.Vector3(   ), 
-                new THREE.Vector3( 0, 10, 0 ), 
-                new THREE.Vector3( 0, 0 , 10 ), 
+                new THREE.Vector3( 0, 10, 0  ),
+                new THREE.Vector3( 0, 0 , 10),
                 new THREE.Vector3( 0, 10, 10)];
-    let closestPoint_thingy = [[0,0,0], [0,0,1], [0,1,1], [0,1,0], [1,0,0], [1,0,1], [1,1,1], [1,1,0]];
     
     // add 4 lines for each point
     let offset = new Vector3(300,0,0);
-    for (let j=0; j<=2; j++) {
-      let sideLines: Array<THREE.Line> = new Array<THREE.Line>();
-      var lineMaterial = new THREE.LineBasicMaterial( { color: 0xff << (j*8)} );
+    for (let i=0; i<=1; i++) {
+      for (let j=0; j<=2; j++) {
+        let sideLines: Array<THREE.Line> = new Array<THREE.Line>();
+        let color = j == 0 ? 0xff0000 : j == 1 ? 0xff : 0xff00;
+        var lineMaterial = new THREE.LineBasicMaterial( { color: color} );
 
-      let vanishingLines = [];
-      let orientedOffset = new Vector3().add(offset).multiplyScalar(Math.pow(-1, closestPoint_thingy[selectedPoint_thingy][j]));
-      vanishingLines.push([points[0], new Vector3().addVectors(points[0], orientedOffset)]);
-      vanishingLines.push([points[1], new Vector3().addVectors(points[1], orientedOffset)]);
-      vanishingLines.push([points[2], new Vector3().addVectors(points[2], orientedOffset)])
-      vanishingLines.push([points[3], new Vector3().addVectors(points[3], orientedOffset)]);
-      console.log(vanishingLines.length);
-      for (let linePoints of vanishingLines) {
-        var geometry = new THREE.BufferGeometry().setFromPoints( linePoints );
-        sideLines.push(new Line(geometry, lineMaterial));
+        let orientedOffset = new Vector3().add(offset).multiplyScalar(Math.pow(-1, i));
+        for (let point of points) {
+          var geometry = new THREE.BufferGeometry().setFromPoints( [point, new Vector3().addVectors(point, orientedOffset)] );
+          sideLines.push(new Line(geometry, lineMaterial));
+        }
+
+        offset = this.shiftVector3(offset);
+        this.lines.push(sideLines);
+        // shift points 
+        points.map(val => {
+          this.shiftVector3(val);
+        });
       }
-
-      offset = this.shiftVector3(offset);
-      this.lines.push(sideLines);
-      // shift points 
-      points.map(val => {
-        this.shiftVector3(val);
-      });
     }
+    this.lines.forEach((lines)=>lines.forEach(line => scene.add(line)));
   }
 
   shiftVector3(vec: Vector3) : Vector3 {
     vec.set(vec.y, vec.z, vec.x);
-    console.log(vec);
     return vec;
   }
 
   addCube(scene: THREE.Scene) {
     var geometry = new THREE.BoxGeometry(10,10,10);
     // geometry.setFromPoints(this.corners);
-      
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+
+    var material = new THREE.MeshLambertMaterial( { color: 0x606060 } );
     this.cube = new THREE.Mesh( geometry, material );
     this.cube.position.set(5,5,5);
 
     scene.add( this.cube );
   }
 
-  generateCorners() {
-    this.corners = new Array<THREE.Vector3>(8);
-    this.corners[0] = new THREE.Vector3(0,0,0);
-    this.corners[1] = new THREE.Vector3(0,10,0);
-    this.corners[2] = new THREE.Vector3(0,10,10);
-    this.corners[3] = new THREE.Vector3(0,0,10);
-    this.corners[4] = new THREE.Vector3(10,0,0);
-    this.corners[5] = new THREE.Vector3(10,10,0);
-    this.corners[6] = new THREE.Vector3(10,10,10);
-    this.corners[7] = new THREE.Vector3(10,0,10);
-  }
-
-  getClosestCornerToCamera() : number {
-    let cameraPosition = this.camera.position;
-    let minDistance = Number.MAX_VALUE;
-    let minDistanceIndex = -1;
-    this.corners.forEach((corner, index) => {
-      let distance = cameraPosition.distanceTo(corner);
-      if (distance < minDistance) {
-        minDistanceIndex = index;
-        minDistance = distance;
-      }
-      // console.log(index, "->", distance);
-    })
-    // debugger;
-    return minDistanceIndex;
-  }
-
   hideLines(closestCorner: number) {
-// 0, 3, 5, 6
-    let closestPoint_thingy = [[0,0,0], [0,0,1], [0,1,1], [0,1,0], [1,0,0], [1,0,1], [1,1,1], [1,1,0]];
+    // 0, 3, 5, 6
+    let closestPoint_thingy = [[0,0,0], [0,1,0], [1,0,0], [1,1,0], [0,0,1], [0,1,1], [1,0,1], [1,1,1]];
 
     closestPoint_thingy[closestCorner].forEach((val, index) => {
       this.lines[index].forEach(line => line.visible = val == 0);
       this.lines[index+3].forEach(line => line.visible = val == 1);
     })
 
+  }
+
+  // camera pos is used to know which corner is closest to it
+  // 0 - x, y, z < 0
+  // 1 - x, y    < 0   z       > 0
+  // 2 - y, z    < 0   x       > 0
+  // 3 - y       < 0   x, z    > 0
+  // 4 - x, z    < 0   y       > 0
+  // 5 - x       < 0   y, z    > 0
+  // 6 -               x, y, z > 0
+  // 7 - y       < 0   x, z    > 0
+
+  getCameraQuadrant() {
+    let cameraPos = this.camera.position;
+    let sx = cameraPos.x > 5 ? 1 : 0 
+    let sy = cameraPos.y > 5 ? 1 : 0
+    let sz = cameraPos.z > 5 ? 1 : 0
+    
+    let index = 4 * sy + 2 * sx + sz
+    return index
   }
 }
